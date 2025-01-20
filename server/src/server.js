29ecdce4afa1aa66e2b653b8eefc23333ed287b0
jssekhar2000 <jssekhar2000@gmail.com>
2025-01-20 21:39:49 +0530

    export app

--- server/src/server.js
+++ server/src/server.js
@@ -4,9 +4,11 @@ const connect = require("./configs/db");
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, async () => {
-    await connect();
-    console.log(`Listening to port ${port}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, async () => {
+        await connect();
+        console.log(`Local server is running on port ${port}`);
+    });
+}
 
 module.exports = app;
